67a7d77ef5e8b9143baacea674aa300bfaa40aa8
Sam Levine <samuel.levine@gmail.com>
2014-01-25 17:06:44 -0500

    Added water fixes!  (Can ray cast through, and not see when fish.)

---
+++ PolymorphGame/Assets/Scripts/FishCharacter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishFocus : MonsterFocus {
+
+	public override bool OnFocus() {
+		// Get all the waters and disable them!!
+		GameObject[] waters = GameObject.FindGameObjectsWithTag ("Water");
+		foreach (GameObject w in waters) {
+			w.SetActive (false);
+		}
+
+		//TODO: CHRIS'S AWESOME FISH STUFF
+	}
---
+++ PolymorphGame/Assets/Scripts/RatCharacter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishFocus : MonsterFocus {
+
+	public override bool OnFocus() {
+		// Get all the waters and disable them!!
+		GameObject[] waters = GameObject.FindGameObjectsWithTag ("Water");
+		foreach (GameObject w in waters) {
+			w.SetActive (true);
+		}
+	}
