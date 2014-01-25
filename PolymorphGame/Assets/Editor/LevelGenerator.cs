3f0ad0e61845ad9d57ff2fbac96e6cb92a19ef3b
Suyog Sonwalkar <Flux159@gmail.com>
2014-01-25 03:49:33 -0500

    Modified Level Generator script. Need to fix up bugs.

--- PolymorphGame/Assets/Editor/LevelGenerator.cs
+++ PolymorphGame/Assets/Editor/LevelGenerator.cs
@@ -7,25 +7,47 @@ public class LevelGenerator : EditorWindow
 
 	public Texture2D texture;
 
-	public Color FloorColor = new Color (128f / 255f, 128f / 255f, 128f / 255f);
-	public Color WallColor = new Color (127f / 255f, 51f / 255f, 0.0f);
+//	public Color FloorColor = new Color (128f / 255f, 128f / 255f, 128f / 255f);
+//	public Color WallColor = new Color (138f / 255f, 43f / 255f, 0.0f);
+//
+//	public Color EnemyRatColor = new Color (72f / 255f, 0.0f, 255f / 255f);
+//	public Color EnemyBatColor = new Color (178f / 255f, 0.0f, 255f / 255f);
+//	public Color EnemyFishColor = new Color (255f / 255f, 0.0f, 220f / 255f);
+//	public Color EnemyStickColor = new Color (255f / 255f, 0.0f, 110f / 255f);
+//
+//	public Color StartColor = new Color (255f / 255f, 216f / 255f, 0.0f);
+//	public Color ExitColor = new Color (182f / 255f, 255f / 255f, 0.0f);
+//
+//	public Color DoorColor = new Color (0.0f, 255f / 255f, 33f / 255f);
+//
+//	public Color PitColor = new Color (0.0f, 0.0f, 0.0f);
+//	public Color WaterColor = new Color (0.0f, 148f / 255f, 255f / 255f);
+//	public Color WaterWithWallColor = new Color (255f, 0.0f, 0.0f);
+//	public Color HoleForRatColor = new Color (255f / 255f, 106f / 255f, 0.0f);
+//	public Color HoleForBatColor = new Color (76f / 255f, 255f / 255f, 0.0f);
+//	public Color ThinWallForStickColor = new Color (0.0f, 127f / 255f, 70f / 255f);
 
-	public Color EnemyRatColor = new Color (72f / 255f, 0.0f, 255f / 255f);
-	public Color EnemyBatColor = new Color (178f / 255f, 0.0f, 255f / 255f);
-	public Color EnemyFishColor = new Color (255f / 255f, 0.0f, 220f / 255f);
-	public Color EnemyStickColor = new Color (255f / 255f, 0.0f, 110f / 255f);
-
-	public Color StartColor = new Color (255f / 255f, 216f / 255f, 0.0f);
-	public Color ExitColor = new Color (182f / 255f, 255f / 255f, 0.0f);
-
-	public Color DoorColor = new Color (0.0f, 255f / 255f, 33f / 255f);
-
+	//This isn't working - check this and the "FloatEquals, NewFloatEquals, and ColorEquals methods"
+	//Along with "GenerateLevel" method
+	public Color FloorColor = new Color (128f, 128f, 128f);
+	public Color WallColor = new Color (138f, 43f, 0f);
+	
+	public Color EnemyRatColor = new Color (72f, 0.0f, 255f);
+	public Color EnemyBatColor = new Color (178f, 0.0f, 255f);
+	public Color EnemyFishColor = new Color (255f, 0.0f, 220f);
+	public Color EnemyStickColor = new Color (255f, 0.0f, 110f);
+	
+	public Color StartColor = new Color (255f, 216f, 0.0f);
+	public Color ExitColor = new Color (182f, 255f, 0.0f);
+	
+	public Color DoorColor = new Color (0.0f, 255f, 33f);
+	
 	public Color PitColor = new Color (0.0f, 0.0f, 0.0f);
-	public Color WaterColor = new Color (0.0f, 148f / 255f, 255f / 255f);
+	public Color WaterColor = new Color (0.0f, 148f, 255f);
 	public Color WaterWithWallColor = new Color (255f, 0.0f, 0.0f);
-	public Color HoleForRatColor = new Color (255f / 255f, 106f / 255f, 0.0f);
-	public Color HoleForBatColor = new Color (76f / 255f, 255f / 255f, 0.0f);
-	public Color ThinWallForStickColor = new Color (0.0f, 127f / 255f, 70f / 255f);
+	public Color HoleForRatColor = new Color (255f, 106f, 0.0f);
+	public Color HoleForBatColor = new Color (76f, 255f, 0.0f);
+	public Color ThinWallForStickColor = new Color (0.0f, 127f, 70f);
 
 	public GameObject wallPrefab;
 	public GameObject floorPrefab;
@@ -169,11 +191,20 @@ public class LevelGenerator : EditorWindow
 		return (((a - EPSILON) < b) && ((a + EPSILON) > b));
 	}
 
+	bool NewFloatEquals (float a, float b)
+	{
+		return ((int)(a * 255f)) == ((int)(b * 255f));
+	}
+
 	bool ColorEquals (Color a, Color b)
 	{
-		if (FloatEquals (a.r, b.r) && FloatEquals (a.g, b.g) && FloatEquals (a.b, b.b)) {
+//		if (FloatEquals (a.r, b.r) && FloatEquals (a.g, b.g) && FloatEquals (a.b, b.b)) {
+//			return true;
+//		}
+		if (NewFloatEquals (a.r, b.r) && NewFloatEquals (a.g, b.g) && NewFloatEquals (a.b, b.b)) {
 			return true;
 		}
+
 		return false;
 	}
 
