7a1b4fdac267fea187aba44a1f7240ecb8954d52
Suyog Sonwalkar <Flux159@gmail.com>
2014-01-25 12:01:40 -0500

    Adding Player Controller Singleton.

---
+++ PolymorphGame/Assets/Scripts/PlayerControllerSingleton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControllerSingleton : MonoBehaviour
+{
+
+		private static PlayerControllerSingleton instance;
+
+		public GameObject playerController;
+
+		private PlayerControllerSingleton ()
+		{
+		}
+	
+		public static PlayerControllerSingleton Instance {
+				get {
+						if (instance == null) {
+								instance = new PlayerControllerSingleton ();
+						}
+						return instance;
+				}
+		}
+}
