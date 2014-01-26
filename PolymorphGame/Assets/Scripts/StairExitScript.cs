147304139fa1b6fcda3385b621d89b19c73a0a96
Suyog Sonwalkar <Flux159@gmail.com>
2014-01-25 23:30:08 -0500

    Updated stairs script.

--- PolymorphGame/Assets/Scripts/StairExitScript.cs
+++ PolymorphGame/Assets/Scripts/StairExitScript.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class StairExitScript : MonoBehaviour {
+public class StairExitScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
+		// Use this for initialization
+		void Start ()
+		{
+		
+		}
+
+		void OnCollisionEnter (Collision collision)
+		{
+				if (collision.transform.tag == "Player") {
+						Application.LoadLevel (Application.loadedLevel + 1);
+				}
+		}
+
+		// Update is called once per frame
+		void Update ()
+		{
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+		}
 }
